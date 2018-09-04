33dad8b704786478bc57b2b312f818bf77a34e5c
Anthony Christe <anthony@localhost.localdomain>
2018-09-04 11:21:41 -1000

    Add spell checking support

---
+++ ci.py
@@ -0,0 +1,25 @@
+import glob
+import subprocess
+import sys
+
+if __name__ == "__main__":
+    tex_files = glob.glob("*.tex")
+    print("Checking spelling of {} tex source files...".format(len(tex_files)))
+    errors = []
+
+    for tex_file in tex_files:
+        result = subprocess.run(["hunspell", "-u3", "-d", "en_US", "-p", "dict.txt", "-t", tex_file], stdout=subprocess.PIPE)
+        stdout = result.stdout
+        if len(stdout) > 0:
+            errs = list(map(lambda line: line.strip(), stdout.strip().split(b"\n")))
+            errors.extend(errs)
+
+    if len(errors) > 0:
+        print("Errors found...")
+        for error in errors:
+            print(error)
+        sys.exit(1)
+    else:
+        print("No errors found...")
+        sys.exit(0)
+
